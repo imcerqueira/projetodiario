a99bf84024f73fe57c02be4adbb67cb761ee5b97
imcerqueira <igormc96@gmail.com>
2024-08-16 11:15:03 -0300

    Commit inicial

---
+++ app/aluno.py
@@ -0,0 +1,6 @@
+class Aluno:
+    def __init__(self, ra, nome, tempoestudo, rendafamiliar) :
+        self.ra = ra # Registro do aluno 
+        self.nome = nome
+        self.tempoestudo = tempoestudo
+        self.rendafamiliar = rendafamiliar
